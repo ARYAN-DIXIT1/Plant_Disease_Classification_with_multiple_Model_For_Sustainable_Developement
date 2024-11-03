3be51b9e4e2498ae78fab845b77eb6210f98383f
ARYAN-DIXIT1 <118048506+ARYAN-DIXIT1@users.noreply.github.com>
2024-11-03 14:40:18 +0530

    adding setup.py file into it

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Plant-disease-classification',
+    version= '0.0.0',
+    author= 'your name',
+    author_email= 'your email id',
+    packages= find_packages(),
+    install_requires = []
+
+)
